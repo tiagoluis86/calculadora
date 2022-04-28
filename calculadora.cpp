ae5558c7f4740058e92df1d7ee8b14172c952796
Tiago <70555750+tiagoluis86@users.noreply.github.com>
2022-04-28 13:21:19 -0300

    Add files via upload

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,46 +1,47 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Calculadora Simples\n";
-    std::cout << "Digite um número: ";
+    cout << "Calculadora Simples\n";
+    cout << "Digite um número: ";
 
     float num{ }; //declara a variável num como int
-    std::cin >> num; //pega o valor da variável num da entrada do usuário
+    cin >> num; //pega o valor da variável num da entrada do usuário
 
-    std::cout << "Digite outro número: ";
+    cout << "Digite outro número: ";
 
     float outronum{ };
-    std::cin >> outronum;
+    cin >> outronum;
 
-    std::cout << "Qual operação você deseja fazer?\n + Soma\n - Subtração\n * Multiplicação\n / Divisão \n";
+    cout << "Qual operação você deseja fazer?\n + Soma\n - Subtração\n * Multiplicação\n / Divisão \n";
 
     char operacao{ };
-    std::cin >> operacao;
+    cin >> operacao;
 
     switch(operacao) {
         case '+': 
-        std::cout << "A soma deu: " << num + outronum << '\n';
-        std::cout << "Operação escolhida: " << operacao << '\n';
+        cout << "A soma deu: " << num + outronum << '\n';
+        cout << "Operação escolhida: " << operacao << '\n';
         break;    
 
         case '-':
-        std::cout << "A subtração deu: " << num - outronum << '\n';
-        std::cout << "Operação escolhida: " << operacao << '\n';
+        cout << "A subtração deu: " << num - outronum << '\n';
+        cout << "Operação escolhida: " << operacao << '\n';
         break;
   
         case '*':
-        std::cout << "A multiplicação deu: " << num * outronum << '\n';
-        std::cout << "Operação escolhida: " << operacao << '\n';
+        cout << "A multiplicação deu: " << num * outronum << '\n';
+        cout << "Operação escolhida: " << operacao << '\n';
         break;
 
         case '/':
-        std::cout << "A divisão deu: " << num / outronum << '\n';
-        std::cout << "Operação escolhida: " << operacao << '\n';
+        cout << "A divisão deu: " << num / outronum << '\n';
+        cout << "Operação escolhida: " << operacao << '\n';
         break;
 
         default:
-        std::cout << "Operação inválida" << '\n';
+        cout << "Operação inválida" << '\n';
     }
 
     return 0;
